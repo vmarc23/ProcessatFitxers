8d29d1f231194fda142c59f32856900043c90b65
vmarc23 <vgmarc2@gmail.com>
2024-03-05 19:03:14 +0100

    Agregar archivos de proyecto.

---
+++ ProcessatFitxers/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace ProcessatFitxers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_fitxers = new System.Windows.Forms.TextBox();
+            this.txt_lletres = new System.Windows.Forms.TextBox();
+            this.btn_codif = new System.Windows.Forms.Button();
+            this.btf_genera = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Número de fitxers";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 63);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Número de lletres";
+            // 
+            // txt_fitxers
+            // 
+            this.txt_fitxers.Location = new System.Drawing.Point(123, 26);
+            this.txt_fitxers.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_fitxers.Name = "txt_fitxers";
+            this.txt_fitxers.Size = new System.Drawing.Size(76, 20);
+            this.txt_fitxers.TabIndex = 2;
+            // 
+            // txt_lletres
+            // 
+            this.txt_lletres.Location = new System.Drawing.Point(123, 63);
+            this.txt_lletres.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_lletres.Name = "txt_lletres";
+            this.txt_lletres.Size = new System.Drawing.Size(76, 20);
+            this.txt_lletres.TabIndex = 3;
+            // 
+            // btn_codif
+            // 
+            this.btn_codif.Location = new System.Drawing.Point(16, 107);
+            this.btn_codif.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_codif.Name = "btn_codif";
+            this.btn_codif.Size = new System.Drawing.Size(82, 50);
+            this.btn_codif.TabIndex = 4;
+            this.btn_codif.Text = "Generar Codificació";
+            this.btn_codif.UseVisualStyleBackColor = true;
+            this.btn_codif.Click += new System.EventHandler(this.btn_codif_Click);
+            // 
+            // btf_genera
+            // 
+            this.btf_genera.Location = new System.Drawing.Point(123, 107);
+            this.btf_genera.Margin = new System.Windows.Forms.Padding(2);
+            this.btf_genera.Name = "btf_genera";
+            this.btf_genera.Size = new System.Drawing.Size(82, 50);
+            this.btf_genera.TabIndex = 5;
+            this.btf_genera.Text = "Generar Fitxers";
+            this.btf_genera.UseVisualStyleBackColor = true;
+            this.btf_genera.Click += new System.EventHandler(this.btf_genera_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 176);
+            this.Controls.Add(this.btf_genera);
+            this.Controls.Add(this.btn_codif);
+            this.Controls.Add(this.txt_lletres);
+            this.Controls.Add(this.txt_fitxers);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_fitxers;
+        private System.Windows.Forms.TextBox txt_lletres;
+        private System.Windows.Forms.Button btn_codif;
+        private System.Windows.Forms.Button btf_genera;
+    }
+}
+
---
+++ ProcessatFitxers/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProcessatFitxers
+{
+    public partial class Form1 : Form
+    {
+        List<string> vocals = new List<string> { "A", "E", "I", "O", "U" };
+        List<Queue<int>> cues = new List<Queue<int>>();
+        Dictionary<string, string> codis = new Dictionary<string, string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Queue<int> GenerarNumerosAleatorios()
+        {
+            ArrayList Nums = new ArrayList() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Queue<int> cola = new Queue<int>();
+            int bitConv;
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                byte[] valor = new byte[4];
+                rng.GetBytes(valor);
+                bitConv = BitConverter.ToInt32(valor, 0);
+            }
+            Random rnd = new Random(bitConv);
+
+            for (int i = 0; i < 10; i++)
+            {
+                int aleatori = rnd.Next(0, Nums.Count);
+                cola.Enqueue((int)Nums[aleatori]);
+                Nums.RemoveAt(aleatori);
+            }
+            return cola;
+        }
+
+        static string Codificar(Queue<int> queue)
+        {
+            List<string> stringList = new List<string>();
+            foreach (int num in queue)
+            {
+                stringList.Add(num.ToString());
+            }
+            return string.Join("", stringList);
+        }
+
+        private void btn_codif_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                var cua = GenerarNumerosAleatorios();
+                cues.Add(cua);
+                codis.Add(Codificar(cua), "");
+            }
+
+            using (FileStream f = new FileStream("codificacio.txt", FileMode.OpenOrCreate))
+            {
+                using (StreamWriter s = new StreamWriter(f))
+                {
+                    for (int i = 0; i < 5; i++)
+                    {
+                        var codi = codis.ElementAt(i); 
+                        s.WriteLine(vocals[i] + ":" + codi.Key); 
+                    }
+                }
+            }
+            MessageBox.Show("Fitxer de codificacio creat correctament");
+        }
+        //FITXERS
+
+        Thread t1;
+        Thread t2;
+        private void btf_genera_Click(object sender, EventArgs e)
+        {
+            if (txt_fitxers.Text != "" && txt_lletres.Text != "")
+            {
+                t1 = new Thread(genArx);
+                t2 = new Thread(comprimirzip);
+
+                int numfitx = int.Parse(txt_fitxers.Text);
+                t1.Start();
+                t2.Start(numfitx);
+
+                MessageBox.Show("Arxius creats correctament");
+            }
+            else
+            {
+                MessageBox.Show("Primer, indica el número de lletres i fitxers.");
+            }
+        }
+
+        private void genArx()
+        {
+            string directori = Path.Combine(Directory.GetCurrentDirectory() + "\\fitxers");
+            DirectoryInfo di = new DirectoryInfo(directori);
+            if (Directory.Exists(directori))
+            {
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo dir in di.GetDirectories())
+                {
+                    dir.Delete(true);
+                }
+            }
+            else
+            {
+                Directory.CreateDirectory(directori);
+            }
+
+            Random number = new Random();
+            int randomnumfitx = int.Parse(txt_fitxers.Text) + 1;
+            int randomnumlletres = int.Parse(txt_lletres.Text) + 1;
+
+            Parallel.For(1, randomnumfitx, numero =>
+            {
+                string nomfitxer = "fitxer" + numero + ".txt";
+                FileStream fs = File.Create(directori + "\\" + nomfitxer);
+                fs.Close();
+            });
+
+            foreach (string fichero in Directory.GetFiles(directori))
+            {
+                FileStream fst = new FileStream(fichero, FileMode.Open, FileAccess.ReadWrite);
+                StreamWriter sw = new StreamWriter(fst);
+
+                for (int i = 0; i < randomnumlletres; i++)
+                {
+                    int randomnumind = number.Next(0, 4);
+                    sw.Write(vocals[randomnumind]);
+                }
+                sw.WriteLine();
+                sw.Close();
+                fst.Close();
+
+                FileStream fst2 = new FileStream(fichero, FileMode.Open, FileAccess.ReadWrite);
+                StreamReader sr = new StreamReader(fst2);
+                StreamWriter sw2 = new StreamWriter(fst2);
+                Dictionary<string, string> codis = getDecodificacio();
+
+                string vocalesArchivo = sr.ReadToEnd();
+
+                foreach (char vocal in vocalesArchivo)
+                {
+                    codis.TryGetValue(vocal.ToString(), out string value);
+                    sw2.Write(value);
+                }
+                sw2.Close();
+                sr.Close();
+                fst2.Close();
+            }
+        }
+
+        private Dictionary<string, string> getDecodificacio()
+        {
+            try
+            {
+                string directori = Path.Combine(Directory.GetCurrentDirectory(), "codificacio.txt");
+                Dictionary<string, string> codis = new Dictionary<string, string>();
+
+                using (FileStream fs = new FileStream(directori, FileMode.Open, FileAccess.Read))
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    char[] delims = new[] { '\r', '\n' };
+                    string keypairs = sr.ReadToEnd();
+                    string[] pairs = keypairs.Split(delims);
+
+                    foreach (string line in pairs)
+                    {
+                        if (line != "")
+                        {
+                            string[] key = line.Split(':');
+                            codis.Add(key[0], key[1]);
+                        }
+                    }
+                }
+                return codis;
+            }
+            catch
+            {
+                MessageBox.Show("No se encuentra el fichero de codificación");
+                return codis;
+            }
+        }
+
+        private void comprimirzip(object nombreArxius)
+        {
+            t1.Join();
+            string path = Path.Combine(Directory.GetCurrentDirectory() + "\\fitxers.zip");
+            string directori = Path.Combine(Directory.GetCurrentDirectory() + "\\fitxers");
+            string[] files = Directory.GetFiles(directori);
+
+            DirectoryInfo di = new DirectoryInfo(Directory.GetCurrentDirectory());
+            foreach (FileInfo file in di.GetFiles())
+            {
+                if (file.Name == "fitxers.zip")
+                {
+                    file.Delete();
+                }
+            }
+
+            using (var archive = ZipFile.Open(path, ZipArchiveMode.Create))
+            {
+                foreach (var fPath in files)
+                {
+                    archive.CreateEntryFromFile(fPath, Path.GetFileName(fPath));
+                }
+            }
+        }
+    }
+}
